f961f207b0b281b9f9cc1c3d2a47858ba9b2407b
danpatrickoneill <danpatrickoneill@gmail.com>
2020-06-02 20:58:11 -0400

    Letter histrogram building properly

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -1,5 +1,23 @@
+import string
 # Use frequency analysis to find the key to ciphertext.txt, and then
 # decode it.
-
+with open("ciphertext.txt") as f:
+  text = f.read()
 # Your code here
+valid_chars = set(string.ascii_lowercase)
+frequencies = {}
+letter_count = 0
+for char in text:
+  char = char.lower()
+  if char in valid_chars:
+    # Increment letter_count for finding percentages
+    letter_count += 1
+    # Set char as key in freq dict; start at 1
+    if char not in frequencies:
+      frequencies[char] = 1
+    # Else, increment count
+    else:
+      frequencies[char] += 1
 
+print(letter_count)
+print(frequencies)
