317a32a08bced1fc29f7c42d5ea673db051403e1
danpatrickoneill <danpatrickoneill@gmail.com>
2020-06-02 21:09:24 -0400

    Decoder dict set up successfully

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -4,11 +4,11 @@ import string
 with open("ciphertext.txt") as f:
   text = f.read()
 # Your code here
-valid_chars = set(string.ascii_uppercase)
+encoded_chars = set(string.ascii_uppercase)
 frequencies = {}
 letter_count = 0
 for char in text:
-  if char in valid_chars:
+  if char in encoded_chars:
     # Increment letter_count for finding percentages
     letter_count += 1
     # Set char as key in freq dict; start at 1
@@ -22,4 +22,9 @@ print(letter_count)
 print(frequencies)
 
 # Probably no need to calculate percentages; lining up the rankings an equally valid option
-
+frequency_ranked_letters = ['E', 'T', 'A', 'O', 'H', 'N', 'R', 'I', 'S', 'D', 'L', 'W', 'U', 'G', 'F', 'B', 'M', 'Y', 'C', 'P', 'K', 'V', 'Q', 'J', 'X', 'Z']
+frequency_tuples = [(letter, frequencies[letter]) for letter in frequencies]
+frequency_tuples.sort(key=lambda tup: tup[1], reverse=True)
+decoder = {letter: frequency_tuples[i][0] for (i, letter) in enumerate(frequency_ranked_letters)}
+print(decoder)
+decoded_text = ''
