98aabff7afa65dcc2da50e2c9fc52b76eb553121
danpatrickoneill <danpatrickoneill@gmail.com>
2020-06-07 20:12:45 -0400

    Fixed bug with dupe same number sum combos

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -3,9 +3,9 @@ find all a, b, c, d in q such that
 f(a) + f(b) = f(c) - f(d)
 """
 
-#q = set(range(1, 10))
+q = set(range(1, 10))
 # q = set(range(1, 200))
-q = (1, 3, 4, 7, 12)
+# q = (1, 3, 4, 7, 12)
 
 
 def f(x):
