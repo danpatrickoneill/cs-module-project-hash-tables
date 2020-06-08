7dfeb0f8cfd1476bdd3f2428a15bb9d721005c4c
danpatrickoneill <danpatrickoneill@gmail.com>
2020-06-07 20:12:13 -0400

    Cleaned up output

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -4,8 +4,8 @@ f(a) + f(b) = f(c) - f(d)
 """
 
 #q = set(range(1, 10))
-q = set(range(1, 200))
-# q = (1, 3, 4, 7, 12)
+# q = set(range(1, 200))
+q = (1, 3, 4, 7, 12)
 
 
 def f(x):
@@ -33,10 +33,10 @@ for elem_one in q_list:
             differences[d] = [(elem_one, elem_two)]
     s = results[elem_one] * 2
 
-    if s in sums:
-        sums[s].append((elem_one, elem_one))
-    else:
-        sums[s] = [(elem_one, elem_one)]
+    # if s in sums:
+    #     sums[s].append((elem_one, elem_one))
+    # else:
+    #     sums[s] = [(elem_one, elem_one)]
     if 0 in differences:
         differences[0].append((elem_one, elem_one))
     else:
@@ -46,5 +46,14 @@ for elem_one in q_list:
 result = []
 for s in sums:
     if s in differences:
-        result.extend(('SUM', sums[s], 'DIFFERENCE', differences[s]))
-print(result)
+        result.append((sums[s], differences[s]))
+
+# print(result)
+for t in result:
+    for combo in t[0]:
+        # print(t)
+        num_one = combo[0]
+        num_two = combo[1]
+        num_three = t[1][0][0]
+        num_four = t[1][0][1]
+        print(f"f({num_one}) + f({num_two}) = {results[num_one] + results[num_two]} = f({num_three}) - f({num_four})")
